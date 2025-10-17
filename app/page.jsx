c2c6dda9fb1588e4414e6ab240aa6ef3f901e203
Nouman <nt50616849@gmail.com>
2025-10-17 10:40:41 +0500

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <button>Click</button>
+      <h1 className="main-heading text-3xl">
+        Hello
+      </h1>
+    </>
+  );
+}
